65885990f3843ca7cbc816cc43306aa0140606ef
Sam Stokes <me@samstokes.co.uk>
2023-11-19 22:53:06 -0800

    create xy grid from 'bottom' point with origin in center

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, keyboard_move_things)
+        .add_systems(Update, position_objects_on_grid)
         .add_systems(Update, move_light)
         .run();
 }
@@ -33,27 +35,29 @@ fn setup(
     });
 
     // cube
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(1.5, 0.5, 1.5),
-        ..default()
-    });
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+            ..default()
+        })
+        .insert(GridCoords::new(1, 0));
 
     // tower
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Box {
-            min_x: -0.5,
-            max_x: 0.5,
-            min_y: -0.5,
-            max_y: 2.5,
-            min_z: -0.5,
-            max_z: 0.5,
-        })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(0.5, 0.5, 1.5),
-        ..default()
-    });
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Box {
+                min_x: -0.5,
+                max_x: 0.5,
+                min_y: -0.5,
+                max_y: 2.5,
+                min_z: -0.5,
+                max_z: 0.5,
+            })),
+            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+            ..default()
+        })
+        .insert(GridCoords::ORIGIN);
 
     // light
     commands.spawn(PointLightBundle {
@@ -62,9 +66,56 @@ fn setup(
     });
 }
 
+fn position_objects_on_grid(mut q: Query<(&mut Transform, &GridCoords)>) {
+    for (mut tx, coords) in &mut q {
+        tx.translation = coords.to_world();
+    }
+}
+
+fn keyboard_move_things(keys: Res<Input<KeyCode>>, mut q: Query<&mut GridCoords>) {
+    let (dx, dy) = if keys.just_pressed(KeyCode::W) {
+        (0, 1)
+    } else if keys.just_pressed(KeyCode::A) {
+        (-1, 0)
+    } else if keys.just_pressed(KeyCode::S) {
+        (0, -1)
+    } else if keys.just_pressed(KeyCode::D) {
+        (1, 0)
+    } else {
+        (0, 0)
+    };
+
+    if dx != 0 || dy != 0 {
+        for mut coords in &mut q {
+            coords.x += dx;
+            coords.y += dy;
+        }
+    }
+}
+
 fn move_light(time: Res<Time>, mut light_tx: Query<&mut Transform, With<PointLight>>) {
     let mut light_tx = light_tx.get_single_mut().unwrap();
     let mut light_pos = &mut light_tx.translation;
     light_pos.x = 3.0 * time.elapsed_seconds().sin();
     light_pos.z = 5.0 * time.elapsed_seconds().cos();
+}
+
+#[derive(Component, Clone, Copy)]
+struct GridCoords {
+    x: i8,
+    y: i8,
+}
+
+impl GridCoords {
+    const ORIGIN: GridCoords = GridCoords { x: 0, y: 0 };
+
+    fn new(x: i8, y: i8) -> Self {
+        Self { x, y }
+    }
+
+    fn to_world(&self) -> Vec3 {
+        // grid xy is world xz (world y is height)
+        // invert y so it starts from the bottom of the screen like I expect
+        Vec3::new(self.x as f32, 0.5, -self.y as f32)
+    }
 }
